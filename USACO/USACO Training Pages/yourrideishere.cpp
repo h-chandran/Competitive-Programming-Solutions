edf9c23c158c8093b6273b0cd826349ea4f91d76
Harish <72057623+h-chandran@users.noreply.github.com>
2023-11-23 13:09:19 -0600

    Create yourrideishere.cpp

---
+++ USACO/USACO Training Pages/yourrideishere.cpp
@@ -0,0 +1,36 @@
+/*
+TASK: ride
+LANG: C++                 
+*/
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    ofstream fout ("ride.out");
+    ifstream fin ("ride.in");
+    string a, b;
+    fin >> a >> b;
+    int u = 1;
+    int f = 1;
+
+    for(int i = 0; i < a.size(); i++){
+        u *= a[i] - 64;
+    }
+    for(int i = 0; i < b.size(); i++){
+        f *= b[i] - 64;
+    }
+
+    u = u % 47;
+    f = f % 47;
+
+    if(u == f){
+        fout << "GO" << endl;
+    } else{
+        fout << "STAY" << endl;
+    }
+    return 0;
+}
