ca4093f82632879a7f9d6008788944780af61f17
Harish <72057623+h-chandran@users.noreply.github.com>
2025-07-05 15:12:10 -0500

    template file for cf

---
+++ Codeforces/Temporary Competition Files/template.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
+#define ll long long
+#define MOD 1000000007
+#define PI 3.141592653589793238462643383
+
+void solve(){
+    
+}
+
+int main(){
+    int t; cin >> t;
+    while(t--){
+        solve();
+    }
+}
