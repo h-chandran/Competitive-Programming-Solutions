768ee204d74b4df6e6dca99b6b5a60a2799b338a
Harish <72057623+h-chandran@users.noreply.github.com>
2025-07-15 18:49:37 -0500

    coin combinations (CSES.FI) - unfinished

---
+++ CSES.fi/Dynamic Programming/Coin_Combinations_I.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <climits>
+
+using namespace std;
+#define ll long long
+#define MOD 1000000007
+
+
+vector<int> coins;
+
+vector<int> arr(2000001, 0);
+ 
+int main(){
+    int n, x; cin >> n >> x;
+    for(int i = 0; i < n; i++){
+        ll c; cin >> c; coins.push_back(c);
+    }
+}
