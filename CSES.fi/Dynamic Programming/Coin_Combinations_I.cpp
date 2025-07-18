debe5762dd034cbf92b8fd2b9ccf9c598311a98b
Harish <72057623+h-chandran@users.noreply.github.com>
2025-07-18 11:04:25 -0500

    Coin Combinations I (CSES.FI)

--- CSES.fi/Dynamic Programming/Coin_Combinations_I.cpp
+++ CSES.fi/Dynamic Programming/Coin_Combinations_I.cpp
@@ -10,11 +10,32 @@ using namespace std;
 
 vector<int> coins;
 
-vector<int> arr(2000001, 0);
- 
+vector<int> arr(1000001, -1);
+
+ll ways(ll x){
+    if(x < 0){
+        return 0;
+    }
+    if(x == 0){
+        return 1;
+    }
+    if(arr[x] > -1){
+        return arr[x];
+    }
+    ll sum = 0;
+    for(ll c : coins){
+        sum += ways(x-c);
+        sum %= MOD;
+    }
+    arr[x] = sum;
+    return sum;
+}
 int main(){
     int n, x; cin >> n >> x;
     for(int i = 0; i < n; i++){
         ll c; cin >> c; coins.push_back(c);
     }
+    cout << ways(x);
+
+
 }
