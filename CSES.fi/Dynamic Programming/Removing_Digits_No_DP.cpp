876f5315ca45a3fec33eada83fe5a1c8b4619c82
Harish <72057623+h-chandran@users.noreply.github.com>
2025-07-20 17:26:07 -0500

    removing digits without dp

---
+++ CSES.fi/Dynamic Programming/Removing_Digits_No_DP.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <climits>
+
+using namespace std;
+#define ll long long
+#define MOD 1000000007
+
+int main(){
+    int n; cin >> n;
+    vector<int> dp(n+1, INT_MAX);
+    for(int i = 0; i < n; i++){
+        
+    }
+
+}
