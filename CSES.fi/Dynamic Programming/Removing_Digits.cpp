5fdb353f4cd528164587244a2128e66cf9efd47a
Harish <72057623+h-chandran@users.noreply.github.com>
2025-07-23 20:29:47 -0500

    removing digits climits

--- CSES.fi/Dynamic Programming/Removing_Digits.cpp
+++ CSES.fi/Dynamic Programming/Removing_Digits.cpp
@@ -9,7 +9,7 @@ using namespace std;
 
 int main(){
     int n; cin >> n;
-    vector<int> dp(n+1, 1e9);
+    vector<int> dp(n+1, INT_MAX);
     dp[0] = 0;
     for(int i = 1; i <= n; i++){
         while(i > 0){
